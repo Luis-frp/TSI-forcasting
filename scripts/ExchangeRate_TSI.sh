5f29fb08382f606f81f456eaf6b55570fb44df87
Luís Felipe <luisfrp741@gmail.com>
2025-07-23 15:01:28 -0300

    Evaluation pra saber qual run foi melhor

--- scripts/ExchangeRate_TSI.sh
+++ scripts/ExchangeRate_TSI.sh
@@ -1,4 +1,6 @@
 for seed in $(seq 0 4); do
   #python -u train.py exchange_rate forecast_univar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv_univar --repr-dims 320 --max-threads 8 --seed ${seed} --eval
-  python -u train.py exchange_rate forecast_multivar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
+  # multivar --- Weather and 
+  python -u ../train.py exchange_rate forecast_multivar --archive forecast_csv --alpha 0.0005 --epochs 20 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --repr-dims 320 --max-threads 8 --seed ${seed} --eval
+
 done
